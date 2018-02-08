baa8f1d650ed6e88ff346e2cafc55a52a8163b5c
Daniel Dunbar <danielqdunbar@gmail.com>
2018-02-08 04:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,5 +66,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     console.log(`Your total cost is $${total()}, which will be charged to card ${cardNumber}.`)
+    cart = []
   }
 }
